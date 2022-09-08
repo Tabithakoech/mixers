f0d5d8f25c8e1b3cf9b73896c5cd3c2fdd5fd9ad
TabithaKoech <tabbyk1718@gmail.com>
2022-09-08 21:15:55 +0300

    added initial files

---
+++ src/App.js
@@ -0,0 +1,72 @@
+// import React, { useEffect, useState } from "react";
+// import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import './App.css';
+// import NavBar from "./components/NavBar";
+// import MainContainer from "./components/MainContainer";
+// import MyCocktails from "./components/MyCocktails";
+
+// const App = () => {
+//   const [cocktails, setCocktails] = useState([]);
+//   const [reviews, setReviews] = useState([]);
+
+  
+// useEffect(() => {
+  
+// //get all cocktails to start
+
+//   fetch('http://localhost:9292/cocktails')
+//     .then(response => response.json())
+//     .then(cocktailsData => { //console.log(cocktailsData)
+//       setCocktails(cocktailsData)
+//     })
+
+// //get all reviews
+
+//   fetch('http://localhost:9292/reviews')
+//     .then(response => response.json())
+//     .then(reviewsData => {  //console.log(reviewsData)
+//       setReviews(reviewsData)
+//       //console.log(reviewsData)
+//     })
+
+// }, [])
+// //Delete Cocktail Card
+
+//   function deleteCocktailCard(id) {
+//     fetch(`http://localhost:9292/cocktails/${id}`, {
+//       method: "DELETE",
+//       headers: {'Accept': 'application/json'}
+//     })
+//     .then(()=> {
+//       let updatedCocktailsArray = cocktails.filter(cocktail => cocktail.id !== id)
+//       setCocktails(updatedCocktailsArray)
+//     })
+//   };
+
+
+
+//   return (
+//     <>
+//       <Router>
+//         <NavBar />
+
+//         <Routes>
+//             <Route 
+//             path='/' 
+//             element={ 
+//               <MainContainer 
+//                 cocktails={cocktails} 
+//                 setCocktails={setCocktails} 
+//                 deleteCocktailCard={deleteCocktailCard}
+//                 reviews={reviews}
+//                 setReviews={setReviews}
+//                 // getIndividualCocktailReviews={getIndividualCocktailReviews}
+//                 /> } />
+//             {/* <Route path='/mycocktails' element={ <MyCocktails />} /> */}
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
