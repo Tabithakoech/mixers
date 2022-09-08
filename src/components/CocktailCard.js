521e02e8178285a00da6595cfde21c6f15075a4d
TabithaKoech <tabbyk1718@gmail.com>
2022-09-08 22:01:56 +0300

    Created a card to hold all the cocktail details to be added and a delete and edit button

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -22,30 +22,30 @@ const CocktailCard = ( {cocktail, deleteCocktailCard, eachReview} ) => {
 
 
 
-//   return(
+  return(
 
-// <Card className='card' style={{ width: "10rem" }}>
-//   <Card.Img variant="top" src={image} />
-//   <Card.Body>
-//     <Card.Title>{cocktail_name}</Card.Title>
-//     <Card.Text>
-//       Category: {category} 
-//     </Card.Text>
-//     <Card.Text>
-//       Glass: {glass}
-//     </Card.Text>
-//       {/* Recent Scores: {score} */}
-//     <div>
-//       Comments:
-//       {comment}
-//     </div>
-//     <Button variant="danger" className='card-button' onClick={()=>deleteCocktailCard(id)}>Delete</Button>
-//     <Button variant="warning" className='card-button'>Edit</Button>
-//   </Card.Body>
-// </Card>
-//   )
-// };
+<Card className='card' style={{ width: "10rem" }}>
+  <Card.Img variant="top" src={image} />
+  <Card.Body>
+    <Card.Title>{cocktail_name}</Card.Title>
+    <Card.Text>
+      Category: {category} 
+    </Card.Text>
+    <Card.Text>
+      Glass: {glass}
+    </Card.Text>
+      {/* Recent Scores: {score} */}
+    <div>
+      Comments:
+      {comment}
+    </div>
+    <Button variant="danger" className='card-button' onClick={()=>deleteCocktailCard(id)}>Delete</Button>
+    <Button variant="warning" className='card-button'>Edit</Button>
+  </Card.Body>
+</Card>
+  )
+};
 
 
 
-// export default CocktailCard;
+export default CocktailCard;
